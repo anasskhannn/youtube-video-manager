b14ce18afe88cc0ed7eeb12d6b147c591e48ff37
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-19 22:14:26 +0530

    Setup for Sqlite Connection

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,87 +1,49 @@
-import json
+import sqlite3
 
-# File path
-filename = 'youtube.txt'
+# Establish Db connecttion
+conn = sqlite3.connect("youtube_videos.db")
 
-# Function to load Data from file/server/db
-def load_data():
-    try:
-        # open the file and return the data in json format
-        with open(f"{filename}",'r') as file:
-            return json.load(file)
+# Cursor to execute the queries
+cur = conn.cursor()
 
-    # If file not found
-    except FileNotFoundError:
-        return []
-        
-# Helper method to save files
-def save_videos_helper(videos):
-    with open(f"{filename}", "w") as file:
-        # Dump the data of add_videos functions to file
-        json.dump(videos, file)
+
+# Create a table in database
+cur.execute("""
+            CREATE TABLE IF NOT EXIST videos
+            id INT PRIMARY KEY,
+            name TEXT NOT NULL,
+            time TEXT NOT NULL
+            """)
 
 # Function to Show all Videos
 def list_all_videos(videos):
-    """Why enumerate?
-    enumerate() -> returns tuple of index and data of video.
-    This will help in update and delete part of the Project"""
     print("\n")
     print("*" * 50) #just for decoration
     print("\n")
-    for index, video in enumerate(videos, start=1):
-        print(f"{index} :- {video["title"]}, Duration {video['duration']} \n")
+
+
     print("\n") 
     print("*" * 50) 
     print("\n") 
 
 # function to add one video
 def add_video(videos):
-    name = input("Enter Video Title: ")
-    time = input("Enter Video Duration: ")
-    videos.append({"title": name, "duration": time})
-    save_videos_helper(videos) #this will save the data
+    pass
     
     
 # Function to update detail of video
 def update_details(videos):
-    # First show the list then asked to choose from it
-    list_all_videos(videos)
-    
-    index = int(input("Enter the Video Number to Update: "))    #this is actual number due to enumerate function
-    
-    # check if the index given is correct 
-    if 1<= index <= len(videos):
-        name = input("Enter Video Title: ")
-        time = input("Enter Video Duration: ")
-        # index-1 because we have started the index from 1 in enumerate
-        videos[index-1] = {"title": name, "duration": time}
-        # Save the updated list
-        save_videos_helper(videos) 
-
-    else:
-        print("Invalid Index Selected")
+    pass
 
 
 
 # Function to Delete Video
-def delete_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the Video Number to Delete: "))  
-    
-    if 1<= index <= len(videos):
-        # delete the videos
-        del videos[index-1]
-        
-        save_videos_helper(videos)
-    else:
-        print("Invalid Index Selected")
+def delete_video():
+    pass
 
   
 
-# Wrapping in main to get to know starting of application, i.e from where our application is starting
 def main():
-    videos= load_data() # to load data from file/server/db
-    # Asking for input
     while True:
         print(" \n Youtube Video Manager | Choose Option From Below \n")
         print("1. List all Youtube Videos")
@@ -90,28 +52,26 @@ def main():
         print("4. Delete a Video")
         print("5. Exit")
         
-        choice = input("Enter Your Choice:")
-        # print(videos) #to check what are in videos
+        choice = input("Enter Your Choice: ")
         
-        # Check the choice
         
         match choice:
             
             # List all Youtube Videos
             case "1":
-                list_all_videos(videos)
+                list_all_videos()
             
             # Add A new Youtube
             case "2":
-                add_video(videos) #this will take list of videos and append new video in it
+                add_video() #this will take list of videos and append new video in it
             
             # Update Details
             case "3":
-                update_details(videos) #we will edit according to the index of that video
+                update_details() #we will edit according to the index of that video
             
             # Delete Video
             case "4":
-                delete_video(videos) #same concept of indexing
+                delete_video() #same concept of indexing
             
             case "5":
                 break
@@ -119,6 +79,9 @@ def main():
             # Default Case 
             case _:
                 print("Invalid Input")
+                
+    # Close the Database Connection After break
+    conn.close()
             
 
 if __name__ == "__main__":
